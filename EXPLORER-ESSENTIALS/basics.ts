0d80e0dfcf9dd75cfb6838c7c544e40bb4b0cb1a
prana <pranaytb777@gmail.com>
2025-03-21 23:54:32 +0530

    Learning the essentials of typescript.

---
+++ EXPLORER-ESSENTIALS/basics.ts
@@ -0,0 +1,4 @@
+let userName:string // this is a variable type a typescript feature.
+// with this feature we can make sure that a specific type of value is being
+// assigned to the variable.
+userName = "Pranay Bhoir" // for type string only string value is allowed.
