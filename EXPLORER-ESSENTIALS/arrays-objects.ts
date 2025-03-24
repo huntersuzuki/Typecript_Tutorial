750a22a4fbde17bc7429cd3133474e72d855f7cc
prana <pranaytb777@gmail.com>
2025-03-24 12:19:20 +0530

    Learned about Array types,Generic Types,Tuples and types for objects

--- EXPLORER-ESSENTIALS/arrays-objects.ts
+++ EXPLORER-ESSENTIALS/arrays-objects.ts
@@ -20,3 +20,61 @@ let possibleResults: [number, number]; // This is called a tuple, it's used when
 
 possibleResults = [1, -1]; // Fixed length of two. No error.
 //possibleResults = [10, 20, 30]; // Exceeds length of two. Error "Type '[number, number, number]' is not assignable to type '[number, number]'."Source has 3 element(s) but target allows only 2. "
+
+//Types in objects
+
+// let user = {
+//   name: "Pranay",
+//   age: 33,
+// }; // Because we provided values by default to the object,typescript
+// automatically infers the object type & in our case,
+//let user: {
+//     name: string;
+//     age: number;
+// } types automatically inferred by typescript.
+
+// We can also explicitly assign types to objects.
+//Example -
+
+let user: {
+  name: string;
+  age: number | string;
+  hobbies: string[];
+  role: {
+    description: string;
+    id: number;
+  };
+} = {
+  name: "Pranay",
+  age: 23,
+  hobbies: ["Kabaddi", "Gaming"],
+  role: {
+    description: "admin",
+    id: 1,
+  },
+};
+
+// One more types is used in typescript which is "{}".
+// It stores any value except null or undefined.
+
+//Example -
+
+// let val: {} = undefined; //Type 'undefined' is not assignable to type '{}'.
+// let val2: {} = null; //Type 'null' is not assignable to type '{}'.
+// But it can store any value aside form null and undefined.
+
+let val: {} = "pranay";
+let vale: {} = [1, 2, "max"];
+// etc.
+
+// There is another type for objects so that we can assign what type should be our keys : objects.
+// That type is called "Record" type which is a generic type used for Objects.
+//Example -
+
+let data: Record<string, number | string>;
+// In this "Record<string, number | string>" type the first type defined the key and the second,
+// defines the value.
+data = {
+  entry: 1,
+  entry2: "2",
+};
