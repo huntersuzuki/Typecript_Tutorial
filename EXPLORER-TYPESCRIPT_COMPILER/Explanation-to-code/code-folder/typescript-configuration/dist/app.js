764fc0a79a7dffd3fc912dd24e566a6bcd02694a
prana <pranaytb777@gmail.com>
2025-04-11 18:20:33 +0530

    Add TypeScript configuration files and initial implementation examples

---
+++ EXPLORER-TYPESCRIPT_COMPILER/Explanation-to-code/code-folder/typescript-configuration/dist/app.js
@@ -0,0 +1,9 @@
+let username;
+username = "pranay";
+console.log(username);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 2));
+export {};
+//# sourceMappingURL=app.js.map
---
+++ EXPLORER-TYPESCRIPT_COMPILER/Explanation-to-code/code-folder/typescript-configuration/src/app.ts
@@ -0,0 +1,13 @@
+import fs from "node:fs";
+
+let username: string;
+
+username = "pranay";
+
+console.log(username);
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+console.log(add(1, 2));
