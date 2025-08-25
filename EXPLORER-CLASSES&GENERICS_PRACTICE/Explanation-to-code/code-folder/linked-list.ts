8391c7b4aab5df1f396f874c3a13443ba30f35e6
prana <pranaytb777@gmail.com>
2025-08-25 17:47:04 +0530

    Add LinkedList implementation and TypeScript configuration file for project setup

---
+++ EXPLORER-CLASSES&GENERICS_PRACTICE/Explanation-to-code/code-folder/linked-list.ts
@@ -0,0 +1,24 @@
+class ListNode<T> {
+  next?: ListNode<T>;
+  constructor(public value: T) {}
+}
+class LinkedList<T> {
+  private root?: ListNode<T>;
+  private length = 0;
+  add(value: T) {
+    const node = new ListNode(value);
+    if (!this.root) {
+      this.root;
+    } else {
+      let current = this.root;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.length++;
+  }
+}
+
+const numberList = new LinkedList<number>();
+const nameList = new LinkedList<string>();
